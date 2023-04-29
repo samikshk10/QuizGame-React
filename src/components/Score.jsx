0fbc27832132d71b5077d5b9e5f6e0c8d21e20b4
samik shk <samik.shakya8848@gmail.com>
2023-04-29 12:21:30 +0545

    tryagain

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -3,12 +3,16 @@ import React from 'react'
 export default function Score({score,questionlength}) {
   return (
     <div className='result'>
-
+      <div className='score-container'>
     <span>
         
     Your Score is : {score || 0}/{questionlength || 10}
 
+
 </span>
+<a href='/quizgame'><button className="btn btn-tryagain">Try Again!!!</button></a>
+
+      </div>
     </div>
   )
 }
