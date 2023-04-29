31e968b56fae43e06f3974ba07cb7da90bcd3a59
samik shk <samik.shakya8848@gmail.com>
2023-04-29 12:50:20 +0545

    adf

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -4,20 +4,6 @@ import { useNavigate } from 'react-router-dom';
 export default function Score({score,questionlength,difficulty,category}) {
   const navigate = useNavigate();
 
-  const startgame = () => {
-    navigate('/quizgame',
-        {
-            state: {
-                difficulty: difficulty,
-                category: category
-            }
-            
-        });
-
-        window.location.reload();
-}
-
-
   return (
     <div className='result'>
       <div className='score-container'>
@@ -25,7 +11,7 @@ export default function Score({score,questionlength,difficulty,category}) {
        
     Your Score is : {score || 0}/{questionlength || 10}
 </span>
- <button onClick={()=>startgame()} className='btn btn-tryagain'>Try Again</button> 
+ {/* <button  className='btn btn-tryagain'>Try Again</button>  */}
       </div>
     </div>
   )
